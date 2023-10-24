473628cb870afb04d73919ee330bc0c8ed65c3f0
Karl Pablo <me@karlpablo.com>
2023-10-24 16:45:36 -0700

    Changed red color again to more vibrant one.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ export default {
         'persian-green': '#2A9D8F',
         'saffron': '#E9C46A',
         'sandy-brown': '#F4A261',
-        'blood-red': '#6B0504',
+        'blood-red': '#D72638',
         'sublime': '#262335',
       },
     },
