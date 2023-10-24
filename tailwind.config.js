b90793d27cb0b27dd10166f80be950347c3b1fca
Karl Pablo <me@karlpablo.com>
2023-10-24 16:40:15 -0700

    Fixed color contrast, replaced burnt-sienna with blood-red.

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ export default {
         'persian-green': '#2A9D8F',
         'saffron': '#E9C46A',
         'sandy-brown': '#F4A261',
-        'burnt-sienna': '#E76F51',
+        'blood-red': '#6B0504',
         'sublime': '#262335',
       },
     },
